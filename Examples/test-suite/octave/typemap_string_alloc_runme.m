wsfulton/swig

Restore custom string allocator support
Restore '%typemaps_string_alloc' using the two-argument copy allocator
fragment interface introduced for '%typemaps_string' in SWIG 4.5.0.

Add runtime coverage for all eight UTL target languages that provide
this interface.

Closes #3531

Assisted-by: OpenCode (GPT-5.6 Sol)

Examples/test-suite/octave/typemap_string_alloc_runme.m
@@ -1,0 +1,8 @@
+typemap_string_alloc
+
+Custom_Reset();
+cvar.text = "one";
+cvar.text = "two";
+
+assert(Custom_WasAllocated());
+assert(Custom_WasDeallocated());

Examples/test-suite/perl5/typemap_string_alloc_runme.pl
@@ -1,0 +1,12 @@
+use strict;
+use warnings;
+use Test::More tests => 4;
+BEGIN { use_ok('typemap_string_alloc') }
+require_ok('typemap_string_alloc');
+
+typemap_string_alloc::Custom_Reset();
+$typemap_string_alloc::text = "one";
+$typemap_string_alloc::text = "two";
+
+ok(typemap_string_alloc::Custom_WasAllocated(), "custom allocator called");
+ok(typemap_string_alloc::Custom_WasDeallocated(), "custom deallocator called");

Examples/test-suite/python/typemap_string_alloc_runme.py
@@ -1,0 +1,10 @@
+import typemap_string_alloc
+
+from swig_test_utils import swig_check
+
+
+typemap_string_alloc.Custom_Reset()
+typemap_string_alloc.cvar.text = "one"
+typemap_string_alloc.cvar.text = "two"
+swig_check(typemap_string_alloc.Custom_WasAllocated(), True)
+swig_check(typemap_string_alloc.Custom_WasDeallocated(), True)

Examples/test-suite/r/typemap_string_alloc_runme.R
@@ -1,0 +1,13 @@
+clargs <- commandArgs(trailing=TRUE)
+source(file.path(clargs[1], "unittest.R"))
+
+dyn.load(paste("typemap_string_alloc", .Platform$dynlib.ext, sep=""))
+source("typemap_string_alloc.R")
+cacheMetaData(1)
+
+Custom_Reset()
+text_set("one")
+text_set("two")
+
+unittest(Custom_WasAllocated(), TRUE)
+unittest(Custom_WasDeallocated(), TRUE)

Examples/test-suite/ruby/typemap_string_alloc_runme.rb
@@ -1,0 +1,9 @@
+require 'swig_assert'
+require 'typemap_string_alloc'
+
+Typemap_string_alloc.Custom_Reset()
+Typemap_string_alloc.text = 'one'
+Typemap_string_alloc.text = 'two'
+
+swig_assert_equal_simple(Typemap_string_alloc.Custom_WasAllocated(), true)
+swig_assert_equal_simple(Typemap_string_alloc.Custom_WasDeallocated(), true)

Examples/test-suite/scilab/typemap_string_alloc_runme.sci
@@ -1,0 +1,10 @@
+exec("swigtest.start", -1);
+
+Custom_Reset();
+text_set("one");
+text_set("two");
+
+checkequal(Custom_WasAllocated(), %t, "Custom_WasAllocated()");
+checkequal(Custom_WasDeallocated(), %t, "Custom_WasDeallocated()");
+
+exec("swigtest.quit", -1);

Examples/test-suite/tcl/typemap_string_alloc_runme.tcl
@@ -1,0 +1,11 @@
+if {[catch {load ./typemap_string_alloc[info sharedlibextension] Typemap_string_alloc} err]} {
+  puts stderr "Could not load shared object:\n$err"
+  exit 1
+}
+
+Custom_Reset
+set text "one"
+set text "two"
+
+if {![Custom_WasAllocated]} { error "custom allocator was not called" }
+if {![Custom_WasDeallocated]} { error "custom deallocator was not called" }

Examples/test-suite/typemap_string_alloc.i
@@ -1,0 +1,85 @@
+// Tests custom allocator and deallocator callbacks for %typemaps_string_alloc.
+%module typemap_string_alloc
+
+#if defined(SWIGUTL)
+
+%include <typemaps/string.swg>
+
+%{
+bool Custom_AllocationCalled = false;
+bool Custom_DeallocationCalled = false;
+%}
+
+%inline %{
+void Custom_Reset() {
+  Custom_AllocationCalled = false;
+  Custom_DeallocationCalled = false;
+}
+
+bool Custom_WasAllocated() {
+  return Custom_AllocationCalled;
+}
+
+bool Custom_WasDeallocated() {
+  return Custom_DeallocationCalled;
+}
+%}
+
+%fragment("Custom_NewCopyCharArray", "header", fragment="<string.h>") {
+SWIGINTERNINLINE char *Custom_NewCopyCharArray(const char *ptr, size_t size) {
+  char *copy = 0;
+  if (size) {
+    copy = %new_array(size, char);
+    if (copy) {
+      memcpy(copy, ptr, size * sizeof(char));
+      Custom_AllocationCalled = true;
+    }
+  }
+  return copy;
+}
+SWIGINTERNINLINE void Custom_DeleteCharArray(char *ptr) {
+  if (ptr)
+    Custom_DeallocationCalled = true;
+  %delete_array(ptr);
+}
+}
+%fragment("Custom_NewCopyCharArray");
+
+%fragment("Custom_AsCharPtrAndSize", "header", fragment="SWIG_AsCharPtrAndSize", fragment="Custom_NewCopyCharArray") {
+SWIGINTERNINLINE int Custom_CopyConvertedCharArray(int res, char **ptr, size_t *size, int *alloc) {
+  if (SWIG_IsOK(res) && ptr) {
+    char *original = *ptr;
+    size_t length = size ? *size : (original ? strlen(original) + 1 : 0);
+    *ptr = original ? Custom_NewCopyCharArray(original, length) : 0;
+    if (alloc && *alloc == SWIG_NEWOBJ)
+      %delete_array(original);
+    if (alloc)
+      *alloc = SWIG_NEWOBJ;
+  }
+  return res;
+}
+%#define Custom_AsCharPtrAndSize(obj, ptr, size, alloc) Custom_CopyConvertedCharArray(SWIG_AsCharPtrAndSize(obj, ptr, size, alloc), ptr, size, alloc)
+}
+%fragment("Custom_AsCharPtrAndSize");
+
+%typemaps_string_alloc(
+  %checkcode(STRING),
+  %checkcode(CHAR),
+  SWIGWARN_TYPEMAP_CHARLEAK_MSG,
+  char,
+  Char,
+  Custom_AsCharPtrAndSize,
+  SWIG_FromCharPtrAndSize,
+  strlen,
+  SWIG_strnlen,
+  Custom_NewCopyCharArray,
+  Custom_DeleteCharArray,
+  "<limits.h>",
+  CHAR_MIN,
+  CHAR_MAX)
+
+%inline %{
+char *text = Custom_NewCopyCharArray("initial", sizeof("initial"));
+%}
+
+#endif

didier-jorand/swig-quickjs

Examples/test-suite/octave/typemap_string_alloc_runme.m
@@ -1,0 +1,8 @@
+typemap_string_alloc
+
+Custom_Reset();
+cvar.some_text = "one";
+cvar.some_text = "two";
+
+assert(Custom_WasAllocated());
+assert(Custom_WasDeallocated());

Examples/test-suite/perl5/typemap_string_alloc_runme.pl
@@ -1,0 +1,12 @@
+use strict;
+use warnings;
+use Test::More tests => 4;
+BEGIN { use_ok('typemap_string_alloc') }
+require_ok('typemap_string_alloc');
+
+typemap_string_alloc::Custom_Reset();
+$typemap_string_alloc::some_text = "one";
+$typemap_string_alloc::some_text = "two";
+
+ok(typemap_string_alloc::Custom_WasAllocated(), "custom allocator called");
+ok(typemap_string_alloc::Custom_WasDeallocated(), "custom deallocator called");

Examples/test-suite/python/typemap_string_alloc_runme.py
@@ -1,0 +1,10 @@
+import typemap_string_alloc
+
+from swig_test_utils import swig_check
+
+
+typemap_string_alloc.Custom_Reset()
+typemap_string_alloc.cvar.some_text = "one"
+typemap_string_alloc.cvar.some_text = "two"
+swig_check(typemap_string_alloc.Custom_WasAllocated(), True)
+swig_check(typemap_string_alloc.Custom_WasDeallocated(), True)

Examples/test-suite/r/typemap_string_alloc_runme.R
@@ -1,0 +1,13 @@
+clargs <- commandArgs(trailing=TRUE)
+source(file.path(clargs[1], "unittest.R"))
+
+dyn.load(paste("typemap_string_alloc", .Platform$dynlib.ext, sep=""))
+source("typemap_string_alloc.R")
+cacheMetaData(1)
+
+Custom_Reset()
+some_text_set("one")
+some_text_set("two")
+
+unittest(Custom_WasAllocated(), TRUE)
+unittest(Custom_WasDeallocated(), TRUE)

Examples/test-suite/ruby/typemap_string_alloc_runme.rb
@@ -1,0 +1,9 @@
+require 'swig_assert'
+require 'typemap_string_alloc'
+
+Typemap_string_alloc.Custom_Reset()
+Typemap_string_alloc.some_text = 'one'
+Typemap_string_alloc.some_text = 'two'
+
+swig_assert_equal_simple(Typemap_string_alloc.Custom_WasAllocated(), true)
+swig_assert_equal_simple(Typemap_string_alloc.Custom_WasDeallocated(), true)

Examples/test-suite/scilab/typemap_string_alloc_runme.sci
@@ -1,0 +1,10 @@
+exec("swigtest.start", -1);
+
+Custom_Reset();
+some_text_set("one");
+some_text_set("two");
+
+checkequal(Custom_WasAllocated(), %t, "Custom_WasAllocated()");
+checkequal(Custom_WasDeallocated(), %t, "Custom_WasDeallocated()");
+
+exec("swigtest.quit", -1);

Examples/test-suite/tcl/typemap_string_alloc_runme.tcl
@@ -1,0 +1,11 @@
+if {[catch {load ./typemap_string_alloc[info sharedlibextension] Typemap_string_alloc} err]} {
+  puts stderr "Could not load shared object:\n$err"
+  exit 1
+}
+
+Custom_Reset
+set some_text "one"
+set some_text "two"
+
+if {![Custom_WasAllocated]} { error "custom allocator was not called" }
+if {![Custom_WasDeallocated]} { error "custom deallocator was not called" }

Examples/test-suite/typemap_string_alloc.i
@@ -1,0 +1,85 @@
+// Tests custom allocator and deallocator callbacks for %typemaps_string_alloc.
+%module typemap_string_alloc
+
+#if defined(SWIGUTL)
+
+%include <typemaps/string.swg>
+
+%{
+bool Custom_AllocationCalled = false;
+bool Custom_DeallocationCalled = false;
+%}
+
+%inline %{
+void Custom_Reset() {
+  Custom_AllocationCalled = false;
+  Custom_DeallocationCalled = false;
+}
+
+bool Custom_WasAllocated() {
+  return Custom_AllocationCalled;
+}
+
+bool Custom_WasDeallocated() {
+  return Custom_DeallocationCalled;
+}
+%}
+
+%fragment("Custom_NewCopyCharArray", "header", fragment="<string.h>") {
+SWIGINTERNINLINE char *Custom_NewCopyCharArray(const char *ptr, size_t size) {
+  char *copy = 0;
+  if (size) {
+    copy = %new_array(size, char);
+    if (copy) {
+      memcpy(copy, ptr, size * sizeof(char));
+      Custom_AllocationCalled = true;
+    }
+  }
+  return copy;
+}
+SWIGINTERNINLINE void Custom_DeleteCharArray(char *ptr) {
+  if (ptr)
+    Custom_DeallocationCalled = true;
+  %delete_array(ptr);
+}
+}
+%fragment("Custom_NewCopyCharArray");
+
+%fragment("Custom_AsCharPtrAndSize", "header", fragment="SWIG_AsCharPtrAndSize", fragment="Custom_NewCopyCharArray") {
+SWIGINTERNINLINE int Custom_CopyConvertedCharArray(int res, char **ptr, size_t *size, int *alloc) {
+  if (SWIG_IsOK(res) && ptr) {
+    char *original = *ptr;
+    size_t length = size ? *size : (original ? strlen(original) + 1 : 0);
+    *ptr = original ? Custom_NewCopyCharArray(original, length) : 0;
+    if (alloc && *alloc == SWIG_NEWOBJ)
+      %delete_array(original);
+    if (alloc)
+      *alloc = SWIG_NEWOBJ;
+  }
+  return res;
+}
+%#define Custom_AsCharPtrAndSize(obj, ptr, size, alloc) Custom_CopyConvertedCharArray(SWIG_AsCharPtrAndSize(obj, ptr, size, alloc), ptr, size, alloc)
+}
+%fragment("Custom_AsCharPtrAndSize");
+
+%typemaps_string_alloc(
+  %checkcode(STRING),
+  %checkcode(CHAR),
+  SWIGWARN_TYPEMAP_CHARLEAK_MSG,
+  char,
+  Char,
+  Custom_AsCharPtrAndSize,
+  SWIG_FromCharPtrAndSize,
+  strlen,
+  SWIG_strnlen,
+  Custom_NewCopyCharArray,
+  Custom_DeleteCharArray,
+  "<limits.h>",
+  CHAR_MIN,
+  CHAR_MAX)
+
+%inline %{
+char *some_text = Custom_NewCopyCharArray("initial", sizeof("initial"));
+%}
+
+#endif